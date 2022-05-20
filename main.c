bd68aecb53ec5b87d7ea11b9ebc112a29a7d2381
LuannFelipe <luannf1020@gmail.com>
2022-05-20 19:51:10 -0300

    inicio

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main() {
+
+    //Nome do arquivo txt para salvar os dados
+    char arquivo[] = {"dados.txt"};
+
+    //Quantidade de dados gerados
+    int qtd = 10;
+
+    //Criar o arquivo txt
+    FILE *dataFile = fopen(arquivo, "w");
+
+    //verifica se houve algum erro na criação do arquivo
+    if(dataFile == NULL)
+    {
+        printf("ERRO");
+
+        //Sair do programa
+        exit(1);
+    } else{
+
+        //Cria uma seed baseada no tempo atual
+        srand(time(NULL));
+
+        //loop para escrever cada dado no arquivo
+        for(int i = 0; i < qtd; i++)
+        {
+            fprintf(dataFile,"%d\n", rand());
+        }
+
+        //fechar o arquivo
+        fclose(dataFile);
+
+        printf("Dados inseridos com sucesso");
+    }
+
+
+    return 0;
+}
